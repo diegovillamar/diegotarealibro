a893261fac2b50b095aad1d0af7d2be20f1c80ba
wmarianochiquito <willychiquito96@outlook.com>
2025-06-15 02:48:47 -0500

    wihle5a100

---
+++ wihle5a110/src/App.java
@@ -0,0 +1,15 @@
+//wihle 5a100**/
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Múltiplos de 5 del 0 al 100:");
+
+        int i = 0;
+        while (i <= 100) {
+            System.out.println(i);
+            i += 5;
+        }
+    }
+}
