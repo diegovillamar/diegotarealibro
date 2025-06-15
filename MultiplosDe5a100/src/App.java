486386ea62748db0169a9bdf5d87fb460523187e
wmarianochiquito <willychiquito96@outlook.com>
2025-06-15 02:39:44 -0500

    MultiplosDe5a100

---
+++ MultiplosDe5a100/src/App.java
@@ -0,0 +1,11 @@
+//multiplos de 5 a 100**/
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Múltiplos de 5 del 0 al 100:");
+
+        for (int i = 0; i <= 100; i += 5) {
+            System.out.println(i);
+        }
+    }
+}
